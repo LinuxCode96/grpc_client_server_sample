2778bb6fd8e9b0d78df304ddbfe34eba0806f1c0
LinuxCode96 <108468055+LinuxCode96@users.noreply.github.com>
2023-09-05 00:02:48 +0300

    set up server and client communication

--- grpc_sample/grpc_client/Program.cs
+++ grpc_sample/grpc_client/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+
+Channel channel = new Channel("localhost:7777", ChannelCredentials.Insecure);
+
+try
+{
+   await channel.ConnectAsync();
+   Console.WriteLine("The client connected successfully  to the server");
+   Console.ReadLine();
+}
+catch(Exception ex)
+{
+    Console.WriteLine("An error has been thrown : " + ex);
+}
+finally
+{
+    if(channel != null)
+    {
+        await channel.ShutdownAsync();
+    }
+}
--- grpc_sample/grpc_server/Program.cs
+++ grpc_sample/grpc_server/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+
+Server server = new Server()
+{
+    Ports = { new ServerPort("localhost", 7777, ServerCredentials.Insecure) },
+    Services = { }
+};
+
+try
+{
+    server.Start();
+    Console.WriteLine("Server is listening to port 7777");
+    Console.ReadLine();
+}
+catch(Exception ex)
+{
+    Console.WriteLine("An error has been thrown : " + ex);
+}
+finally
+{
+    if (server is not null)
+    {
+       await server.ShutdownAsync();
+    }
+}
